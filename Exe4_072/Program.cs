620ddd7d03b55433e29d76ea2450855a64e94010
Adnurhytia <adindanurhayati30@gmail.com>
2022-12-09 15:41:34 +0700

    Memberi variabel pada array

--- Exe4_072/Program.cs
+++ Exe4_072/Program.cs
@@ -4,6 +4,10 @@ namespace ExerciseStackUsingLinkedList
 {
     class Stack
     {
-        
+        private string[] dinda;
+        private int top;
+        private int max;
+        public Stack(int size)
+        { }
     }
 }
