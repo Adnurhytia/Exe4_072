71db9e109f3f266a126b958e3372ab26a8e1994c
Adnurhytia <adindanurhayati30@gmail.com>
2022-12-09 15:42:43 +0700

    Memberi index min dan max pada ukuran array

--- Exe4_072/Program.cs
+++ Exe4_072/Program.cs
@@ -8,6 +8,10 @@ namespace ExerciseStackUsingLinkedList
         private int top;
         private int max;
         public Stack(int size)
-        { }
+        {
+            dinda = new string[29];
+            top = -1;
+            max = 29;
+        }
     }
 }
